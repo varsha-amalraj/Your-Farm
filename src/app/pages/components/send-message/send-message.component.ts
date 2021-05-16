904252ba908527d152d7d49c76c8c85b307d9b27
varshaRaj <minivarsha.a@netcon.in>
2021-05-16 13:08:50 +0530

    test

--- src/app/pages/components/send-message/send-message.component.ts
+++ src/app/pages/components/send-message/send-message.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./send-message.component.css']
 })
 export class SendMessageComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
